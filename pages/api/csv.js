58b280687f618e09e78d1944f017b074816a4349
Menilek Techane <menilek25@hotmail.co.uk>
2022-10-13 17:40:35 +0100

    Handle Post & Options request

--- pages/api/csv.js
+++ pages/api/csv.js
@@ -20,15 +20,15 @@ export default async function handler(req, res) {
   const { method } = req;
   await dbConnect();
 
-  switch (method) {
-    case "POST":
+  if (method === "POST") {
       try {
-        const reqWords = req.body.words;
+        const reqWords = req.body;
         await Word.insertMany(reqWords);
-        return res.status(200).json({ message: 'Successful inserted new word(s)' });
+        return res.status(200).json(reqWords).end();
       } catch (error) {
-        return res.status(500).send(error.message || error)
+        return res.status(500).send(error.message || error).end();
       }
-      break
+  } else if (method === "OPTIONS") {
+    return res.status(200).end();
   }
 }
