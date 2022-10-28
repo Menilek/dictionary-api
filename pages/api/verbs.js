6accb1daf562347d56fed0dc0c8e752434e3b295
Menilek Techane <menilek25@hotmail.co.uk>
2022-10-28 08:43:40 +0100

    Add Verbs endpoint

---
+++ pages/api/verbs.js
@@ -0,0 +1,22 @@
+import dbConnect from '../../lib/dbConnect';
+import Word from "../../models/word";
+
+export default async function handler(req, res) {
+  const { method } = req;
+  await dbConnect();
+
+  switch (method) {
+    case 'GET':
+      try {
+        const words = await Word.find({category: "Verb"}, '-__v')
+          .sort({ '_id': -1 });
+
+        console.log(words);
+
+        res.status(200).json({ success: true, data: words });
+      } catch (error) {
+        res.status(500).send(error.message || error)
+      }
+      break
+  }
+}
